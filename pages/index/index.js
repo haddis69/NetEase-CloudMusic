36b18dd882f344fc4c56e790e4314e6430959711
kxy <1474154683@qq.com>
2022-05-17 16:36:11 +0800

    轮播图和个人推荐的动态渲染

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,21 @@
+import request from '../../utils/request'
 // index.js
 // 获取应用实例
 const app = getApp()
 
 Page({
   data: {
-   
+   bannerList:[],
+   recommendList:[]
   },
-  onLoad() {
-
+  onLoad:async function() {
+    let bannerListData = await request('/banner',{type:2});
+    this.setData({
+      bannerList:bannerListData.banners
+    })
+    let recommendListData=await request('/personalized',{limit:10});
+    this.setData({
+      recommendList:recommendListData.result
+    })
   }
 })
